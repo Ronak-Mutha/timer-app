bf9e1f114fa19bec8918588ece533d5d0b8314ae
Ronak Mutha <mutharonak97@gmail.com>
2021-11-15 19:30:23 +0530

    Added basic timer functionality and UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import Timer from "./Components/Timer";
 
 function App() {
+  const hoursMinSecs = { hours: 0, minutes: 0, seconds: 5 };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="timer">
+        <Timer hoursMinSecs={hoursMinSecs} />
+      </div>
     </div>
   );
 }
